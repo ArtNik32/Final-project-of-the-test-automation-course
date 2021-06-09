20867133488c3bd0f9b2fd99f6e2d6bca0483613
ArtNik32 <shah-1@mail.ru>
2021-06-09 16:44:49 +0300

    LoginPage - проверка url страницы, наличия форм входа и регистрации

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+class LoginPageLocators():
+    LOGIN_PAGE_ENTER_FORM = (By.CLASS_NAME, 'col-sm-6.login_form')
+    LOGIN_PAGE_REGISTRATION_FORM = (By.CSS_SELECTOR, '#register_form')
